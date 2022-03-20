a69dcfc51e983523be2e5d8eaf367b56e6fc44c3
MyHASS <fsa317@gmail.com>
2022-03-20 14:28:05 +0000

    whereis

--- __init__.py
+++ __init__.py
@@ -27,6 +27,25 @@ class MyHomeAssistant(MycroftSkill):
     def handle_temp_check_intent(self, message):
         resp = self.getState('sensor.jarvis_temps')
         self.speak(resp)
+
+    @intent_file_handler('everyone.intent')
+    def handle_everyone_intent(self, message):
+        # self.speak("Checking, please hold.")
+        resp1 = self.getState('sensor.jarvis_sal_status')
+        resp2 = self.getState('sensor.jarvis_vincent_status')
+        resp3 = self.getState('sensor.jarvis_tiffany_status')
+        resp4 = self.getState('sensor.jarvis_frank_status')
+        self.speak(resp1)
+        self.speak(resp2)
+        self.speak(resp3)
+        self.speak(resp4)
+
+    @intent_file_handler('whereis.intent')
+    def handle_whereis_intent(self, message):
+        self.log.info("Whereis - "+str(message))
+        pers = message.data["person"]
+        resp = self.getState('sensor.jarvis_'+pers+'_status')
+        self.speak(resp)
     
     def getState(self,entity):
         response = self.callApi('/states/{}'.format(entity))
